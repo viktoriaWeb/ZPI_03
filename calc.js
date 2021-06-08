3e73a3b68aec7a92903f7aa72d1c6d2dd22c6751
ViktoriaPikh <viktoria.pikh@gmail.com>
2021-06-08 23:11:59 +0300

    added implementation

--- calc.js
+++ calc.js
@@ -1,5 +1,32 @@
-function aTaylor(/*in*/iCount){
-return 8
+
+function aTaylor(x, iCount=10){
+    let res = sinh(x, iCount);
+    return strip(res);
+
+
 };
+
+function sinh(x, iternumber) {
+    var sum = 0;
+    for (let i=1; i<iternumber; i++) {
+        sum += (x^(2*i+1))/factorial(2*i+1);
+        console.log(`sum is: ${sum}`); 
+    }
+    
+    return sum;
+};
+
+function factorial(num)
+{
+    var rval=1;
+    for (var i = 2; i <= num; i++)
+        rval = rval * i;
+    return rval;
+};
+
+function strip (number, precision=3) {
+    return (parseFloat(number).toPrecision(precision));
+}
+
  
 module.exports.Taylor = aTaylor; 
